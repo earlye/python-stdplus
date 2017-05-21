7f126fd9b47e56a9801afae77ff78e1bdff6a2f3
Early Ehlinger <earlye@gmail.com>
2017-05-21 07:15:05 -0500

    Refactored stdplus out of nephele

---
+++ tests/stdplus/test.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import sys
+
+from nose.core import run
+from nose.core import unittest
+
+from pprint import pprint
+
+from stdplus._sshConfig import parseSshConfig
+from stdplus import *
+
+def test_isIp():
+    assert(not isIp( "Obviously not an ip" )) # obvious
+    assert(not isIp( "16" )) # obvious
+    assert(not isIp( "1234.123.123.123" )) # first octet is too long
+    assert(not isIp( "123.1234.123.123" )) # second octet is too long
+    assert(not isIp( "123.123.1234.123" )) # third octet is too long
+    assert(not isIp( "123.123.123.1234" )) # fourth octet is too long
+    assert(not isIp( "12a.123.123.123" )) # first octet contains alpha
+    assert(not isIp( "123.12a.123.123" )) # second octet contains alpha
+    assert(not isIp( "123.123.12a.123" )) # third octet contains alpha
+    assert(not isIp( "123.123.123.12a" )) # fourth octet contains alpha
+    assert(not isIp( "192.168.1.1.32" )) # too many octets
+    assert(not isIp( "foo.192.168.1.1" )) # too many octets, leading octet not even a number
+    assert(isIp( "8.8.8.8" )) # all octets single digits
+    assert(isIp( "192.168.100.200" )) # all octets 3 digits
