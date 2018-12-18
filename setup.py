d4e233d637db24da52afb63b6e2d137993ce41f4
earlye <earlye@gmail.com>
2018-12-18 13:40:09 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.45',
+    version='0.0.47',
 
     description='A collection of utilities for python',
     long_description=long_description,
